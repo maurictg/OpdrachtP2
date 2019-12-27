2c20fbecd6ceb94c7ae561c0dee95ebc9d695fc8
AartPieterse <aartpieterse01@gmail.com>
2019-12-27 20:07:11 +0100

    GUI verder gemaakt
    -- een homescreen met een aantal genres
    -- een 'account aanmaken' pagina met textvakken voor gebruikerinformatie
       bij de textvakken 'phonenumber' en 'age' kunnen alleen cijfers worden ingevoerd

--- NetFlix/src/netflix/app/Home.java
+++ NetFlix/src/netflix/app/Home.java
@@ -2,17 +2,116 @@ package netflix.app;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Home extends Application {
 
+    private Stage stage;
+
     @Override
     public void start(Stage stage) {
-        BorderPane bp = new BorderPane();
+        this.stage = stage;
 
-        Scene screen = new Scene(bp, 600, 400);
-        stage.setScene(screen);
+        this.homeScreen();
+    }
+
+    // Hieronder alle screens, ze hebben in de code allemaal dezelfde opbouw
+    // Voor elk genre is er een aparte screen
+
+    private void homeScreen() {
+        BorderPane bPane = new BorderPane();
+
+        //Onderdelen maken
+        Label genreLabel = new Label("Home");
+
+        // Onderdelen toevoegen aan BorderPane
+        bPane.setLeft(getLeftBorder());
+        bPane.setCenter(genreLabel);
+
+        // Deze scene tonen - door method 'run()'
+        this.run(new Scene(bPane, 600, 400));
+    }
+
+    private void actionScreen() {
+        BorderPane bPane = new BorderPane();
+
+        bPane.setLeft(getLeftBorder());
+        bPane.setCenter(new Label("action"));
+
+        this.run(new Scene(bPane, 600, 400));
+    }
+
+    private void comedyScreen() {
+        BorderPane bPane = new BorderPane();
+
+        bPane.setLeft(getLeftBorder());
+        bPane.setCenter(new Label("comedy"));
+
+        this.run(new Scene(bPane, 600, 400));
+    }
+
+    private void scifiScreen() {
+        BorderPane bPane = new BorderPane();
+
+        bPane.setLeft(getLeftBorder());
+        bPane.setCenter(new Label("scifi"));
+
+        this.run(new Scene(bPane, 600, 400));
+    }
+
+    private void horrorScreen() {
+        BorderPane bPane = new BorderPane();
+
+        bPane.setLeft(getLeftBorder());
+        bPane.setCenter(new Label("horror"));
+
+        this.run(new Scene(bPane, 600, 400));
+    }
+
+    private void thrillerScreen() {
+        BorderPane bPane = new BorderPane();
+
+        bPane.setLeft(getLeftBorder());
+        bPane.setCenter(new Label("thriller"));
+
+        this.run(new Scene(bPane, 600, 400));
+    }
+
+    // De Left Border uit de Borderpane, deze wordt in elke screen aangeroepen
+    private Pane getLeftBorder() {
+        VBox vBox = new VBox();
+
+        Button homeButton = newButton("Homepage");
+        Button actionButton = newButton("Action");
+        Button comedyButton = newButton("Comedy");
+        Button scifiButton = newButton("Sci-Fi");
+        Button horrorButton = newButton("Horror");
+        Button thrillerButton = newButton("Thriller");
+
+        vBox.getChildren().addAll(homeButton, actionButton, comedyButton, scifiButton, horrorButton, thrillerButton);
+
+        vBox.setSpacing(6);
+
+        homeButton.setOnAction((event -> homeScreen()));
+        actionButton.setOnAction((event -> actionScreen()));
+        comedyButton.setOnAction((event -> comedyScreen()));
+        scifiButton.setOnAction((event -> scifiScreen()));
+        horrorButton.setOnAction((event -> horrorScreen()));
+        thrillerButton.setOnAction((event -> thrillerScreen()));
+
+        return vBox;
+    }
+
+    private Button newButton(String text) {
+
+        return new Button(text);
+    }
+
+    private void run(Scene scene) {
+
+        stage.setScene(scene);
         stage.setTitle("Netflix Statistics");
         stage.show();
     }
--- NetFlix/src/netflix/app/Login.java
+++ NetFlix/src/netflix/app/Login.java
@@ -1,6 +1,8 @@
 package netflix.app;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -10,13 +12,21 @@ import javafx.stage.Stage;
 
 public class Login extends Application {
 
+    private Stage stage;
     private Home home = new Home();
 
     public void start(Stage stage) {
+        this.stage = stage;
+
+        this.login();
+    }
+
+    private void login() {
         GridPane gPane = new GridPane();
 
         // Alle verschillende onderdelen maken
-        Label usernameLabel = new Label("Username: ");
+        Label headerLabel = new Label("LOGIN");
+        Label usernameLabel = new Label("Emailaddress: ");
         Label passwordLabel = new Label("Password: ");
         Label returnLabel = new Label(" ");
 
@@ -24,13 +34,16 @@ public class Login extends Application {
         TextField passwordField = new TextField();
 
         Button loginButton = new Button("Login");
+        Button accountButton = new Button("Create account");
 
         // Alle onderdelen toevoegen aan de GridPane
-        gPane.add(usernameLabel, 1, 1);
-        gPane.add(passwordLabel, 1, 2);
-        gPane.add(usernameField, 2, 1);
-        gPane.add(passwordField, 2, 2);
-        gPane.add(loginButton, 2, 3);
+        gPane.add(headerLabel, 1, 1);
+        gPane.add(usernameLabel, 1, 2);
+        gPane.add(passwordLabel, 1, 3);
+        gPane.add(usernameField, 2, 2);
+        gPane.add(passwordField, 2, 3);
+        gPane.add(loginButton, 2, 4);
+        gPane.add(accountButton, 2, 5);
 
         // geometry
         gPane.setVgap(15);
@@ -38,22 +51,90 @@ public class Login extends Application {
 
         // De werking van de buttons
         loginButton.setOnAction(event -> {
-            if (checkPassword()) {
-
-                try {
-                    this.home.start(stage);
-                }
-                catch(Exception e) {
-                    returnLabel.setText("Dit ging fout");
-                }
-
+            if (this.checkPassword()) {
+                this.home.start(stage);
             }
 
             returnLabel.setText("Wrong username or password, try again");
         });
 
-        Scene loginScreen = new Scene(gPane, 600, 400);
-        stage.setScene(loginScreen);
+        accountButton.setOnAction(event -> this.createAccount());
+
+        // Deze pagina laten zien d.m.v. de methode 'run()'
+        this.run(new Scene(gPane, 600, 400));
+    }
+
+    private void createAccount() {
+        GridPane grid = new GridPane();
+
+        Label headerLabel = new Label("CREATE ACCOUNT");
+        Label firstnameLabel = new Label("First name: ");
+        Label lastnameLabel = new Label("Last name: ");
+        Label emailLabel = new Label("Emailaddress: ");
+        Label passwordLabel = new Label("Password: ");
+        Label phoneLabel = new Label("Phonenumber: ");
+        Label ageLabel = new Label("Age: ");
+
+        TextField firstnameText = new TextField();
+        TextField lastnameText = new TextField();
+        TextField emailText = new TextField();
+        TextField passwordText = new TextField();
+        TextField phoneText = new TextField();
+        TextField ageText = new TextField();
+
+        Button createButton = new Button("Create Account");
+
+        grid.add(headerLabel, 1, 1);
+        grid.add(firstnameLabel, 1, 2);
+        grid.add(firstnameText, 2, 2);
+        grid.add(lastnameLabel, 1, 3);
+        grid.add(lastnameText, 2, 3);
+        grid.add(emailLabel, 1, 4);
+        grid.add(emailText, 2, 4);
+        grid.add(passwordLabel, 1, 5);
+        grid.add(passwordText, 2, 5);
+        grid.add(phoneLabel, 1, 6);
+        grid.add(phoneText, 2, 6);
+        grid.add(ageLabel, 1, 7);
+        grid.add(ageText, 2, 7);
+        grid.add(createButton, 2, 8);
+
+        grid.setHgap(2);
+        grid.setVgap(5);
+
+        ageText.lengthProperty().addListener(new ChangeListener<>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                if (newValue.intValue() > oldValue.intValue()) {
+                    char ch = ageText.getText().charAt(oldValue.intValue());
+
+                    if (!(ch >= '0' && ch <= '9')) {
+
+                        ageText.setText(ageText.getText().substring(0, ageText.getText().length() - 1));
+                    }}}
+        });
+
+        phoneText.lengthProperty().addListener(new ChangeListener<>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                if (newValue.intValue() > oldValue.intValue()) {
+                    char ch = phoneText.getText().charAt(oldValue.intValue());
+
+                    if (!(ch >= '0' && ch <= '9')) {
+
+                        phoneText.setText(phoneText.getText().substring(0, phoneText.getText().length() - 1));
+                    }}}
+        });
+
+        createButton.setOnAction(event -> this.login());
+
+        this.run(new Scene(grid, 600, 400));
+    }
+
+    private void run(Scene scene) {
+        stage.setScene(scene);
         stage.setTitle("Netflix Statistics");
         stage.show();
     }
