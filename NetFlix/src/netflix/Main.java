6a2be0ef80a58e75566938fd52f78841c6251d96
MaurICT <31762519+maurictg@users.noreply.github.com>
2020-01-04 16:09:26 +0100

    Switch naar FXML

--- NetFlix/src/netflix/Main.java
+++ NetFlix/src/netflix/Main.java
@@ -1,12 +1,36 @@
 package netflix;
 
-import netflix.app.Login;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import netflix.controllers.LoginController;
 
-public abstract class Main extends Application {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        try{
+            //Laad de login pagina in (FXML)
+            FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/Login.fxml"));
+            Parent root = loader.load();
+            LoginController controller = loader.getController();
+
+            //Sla de stage op in de controller zodat we van pagina kunnen wisselen
+            controller.setStage(stage);
+
+            stage.setTitle("Netflix Statistix");
+            stage.setScene(new Scene(root, 600, 400));
+            stage.show();
+
+        }catch (Exception e){
+            System.out.println("NOT FOUND");
+        }
+    }
 
     public static void main(String[] args) {
-
-        Application.launch(Login.class);
+        launch(args);
     }
 }
