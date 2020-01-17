6c04515754a7dccfc2cfb232fb3f2b9c50abea12
Lauran van Ekeren <lauranvanekeren@gmail.com>
2020-01-17 21:12:17 +0100

    verder gewerkt aan watchedprogram

--- NetFlix/src/netflix/app/AccountManager.java
+++ NetFlix/src/netflix/app/AccountManager.java
@@ -2,9 +2,11 @@ package netflix.app;
 
 import netflix.models.Account;
 import netflix.models.Profile;
+import netflix.models.Program;
 
 public class AccountManager {
     public static Account selected;
     public static boolean isEdit;
     public static Profile selectedProfile;
+    public static Program selectedProgram;
 }
--- NetFlix/src/netflix/controllers/WatchProgramController.java
+++ NetFlix/src/netflix/controllers/WatchProgramController.java
@@ -1,9 +1,16 @@
 package netflix.controllers;
 
+import com.maurict.orm.Database;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import netflix.app.AccountManager;
+import netflix.models.Account;
 import netflix.models.Film;
 import netflix.models.Program;
+import netflix.models.WatchedProgram;
 
 import java.util.ArrayList;
 
@@ -12,26 +19,61 @@ public class WatchProgramController extends Controller {
     private ArrayList<Object> programs;
     private ArrayList<Object> films;
     private ArrayList<Integer> programIdsFromFilms;
+    private ArrayList<Object> watchedPrograms;
 
     @FXML
     private ComboBox cbFilms;
 
     @FXML
-    public void cbFilms_Change(){
+    private Label labelMessage;
 
+    @FXML
+    private Button btnWatchFilm;
+
+    @FXML
+    private TextField textFieldMinutesWatched;
+
+    @FXML
+    private void btnWatchFilm_Click(){
+        try {
+            labelMessage.setVisible(false);
+            int minutesWatched = Integer.parseInt(textFieldMinutesWatched.getText());
+            if (minutesWatched >= 0){
+                Database db = Database.global;
+                WatchedProgram watchedProgram = new WatchedProgram();
+                watchedProgram.profileId = AccountManager.selectedProfile.profileId;
+                watchedProgram.programId = AccountManager.selectedProgram.programId;
+                watchedProgram.timeWatched = minutesWatched;
+                db.add(watchedProgram);
+                }
+
+        } catch (Exception e){
+            labelMessage.setVisible(true);
+            e.printStackTrace();
+        }
     }
+
+    @FXML
+    public void cbFilms_Change(){
+        for (Object program : programs) {
+            Program a = (Program)program;
+            if(a.title.equals((String)cbFilms.getValue())){
+                AccountManager.selectedProgram = a;
+                btnWatchFilm.setVisible(true);
+            }
+        }
+    }
+
 
     @Override
     void onLoad() {
-        System.out.println("-1");
+        labelMessage.setVisible(false);
+        btnWatchFilm.setVisible(false);
         try{
-            System.out.println("0");
             programs = new Program().select().toList();
-            System.out.println("0.5");
             films = new Film().select().toList();
-            System.out.println("1");
+            watchedPrograms = new Film().select().toList();
             programIdsFromFilms = new ArrayList<>();
-            System.out.println("2");
             for (Object o: films){
                 programIdsFromFilms.add(((Film)o).programId);
             }
