a5ab0158b027d1b6b28de44c9f8ae729e9acba96
AartPieterse <aartpieterse01@gmail.com>
2020-01-18 18:59:56 +0100

    Viewscontroller

--- NetFlix/src/netflix/Main.java
+++ NetFlix/src/netflix/Main.java
@@ -4,7 +4,6 @@ import com.maurict.orm.Database;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import netflix.controllers.Controller;
-import netflix.models.Film;
 
 public class Main extends Application {
 
@@ -12,7 +11,6 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         //Create connection to the database
         Database db = new Database("localhost","netflix");
-        System.out.println(Film.longestFilmAgeSixteen());
 
         //Create a controller, just because we need to start the gui
         Controller c = new Controller();
--- NetFlix/src/netflix/controllers/ViewsController.java
+++ NetFlix/src/netflix/controllers/ViewsController.java
@@ -1,8 +1,49 @@
 package netflix.controllers;
 
+import com.maurict.orm.Database;
 import javafx.fxml.FXML;
+import netflix.models.Film;
+import javafx.scene.control.*;
 
 public class ViewsController extends Controller {
+
+    Database db = Database.global;
+
+    @FXML
+    private TextArea tbData;
+
+    @FXML
+    private void btnViewAvgWatchedPerEpisode_Click() {
+
+    }
+
+    @FXML
+    private void btnViewAvgWatchedPerEpisodePerAccount_Click() {
+
+    }
+
+    @FXML
+    private void btnViewWatchedMoviesPerAccount_Click() {
+
+    }
+
+    @FXML
+    private void btnViewLongestMovieSixteenYears_Click() {
+
+        Film film = new Film();
+
+        //tbData.setText(film.longestFilmAgeSixteen());
+    }
+
+    @FXML
+    private void btnViewAccountsWithOneProfile_Click() {
+
+    }
+
+    @FXML
+    private void btnViewTimesWatched_Click() {
+
+    }
 
     @FXML
     private void btnBack_Click() {
