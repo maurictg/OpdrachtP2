5c16a065803037347c6601cb39758ecbe6539b41
AartPieterse <aartpieterse01@gmail.com>
2020-01-18 20:49:41 +0100

    Update ViewsController.java

--- NetFlix/src/netflix/controllers/ViewsController.java
+++ NetFlix/src/netflix/controllers/ViewsController.java
@@ -2,8 +2,10 @@ package netflix.controllers;
 
 import com.maurict.orm.Database;
 import javafx.fxml.FXML;
-import netflix.models.Film;
-import javafx.scene.control.*;
+import javafx.scene.control.TextArea;
+import netflix.models.*;
+
+import java.util.ArrayList;
 
 public class ViewsController extends Controller {
 
@@ -25,6 +27,8 @@ public class ViewsController extends Controller {
     @FXML
     private void btnViewWatchedMoviesPerAccount_Click() {
 
+
+
     }
 
     @FXML
@@ -32,17 +36,54 @@ public class ViewsController extends Controller {
 
         Film film = new Film();
 
-        //tbData.setText(film.longestFilmAgeSixteen());
+        tbData.setText("Longest film: \n" + film.longestFilmAgeSixteen());
     }
 
     @FXML
     private void btnViewAccountsWithOneProfile_Click() {
 
+        try {
+
+            ArrayList<Object> accounts = new Account().select().toList();
+            ArrayList<Account> counter = new ArrayList<>();
+
+            for (int i = 0; i < accounts.size(); i++) {
+                if (((Account) accounts.get(i)).profileCount() == 1) {
+                    counter.add((Account) accounts.get(i));
+                }
+            }
+
+            if (!counter.isEmpty()) {
+                for (Account i : counter) {
+                    tbData.setText(i.accountName);
+                }
+
+            } else {
+                tbData.setText("No accounts matched this requirement");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     private void btnViewTimesWatched_Click() {
 
+        try {
+
+
+        ArrayList<Object> films = new WatchedProgram().select().toList();
+        ArrayList<Program> watchedPrograms = new ArrayList<>();
+
+        for (int i = 0; i < films.size(); i++) {
+
+            watchedPrograms.add((Program) films.get(i));
+            tbData.setText(films.get(i));
+        }
+    } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
